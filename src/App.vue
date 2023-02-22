ede813cd42df661e3073f0f6f9f7c2b670f71ce4
Filipe Oliveira <61127389+filipeoliveira93@users.noreply.github.com>
2023-02-21 21:54:46 -0300

    new params api

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
-<template>
-  <div>
-    <Geolocation />
-  </div>
-</template>
-
-<script>
-import Geolocation from '/src/components/Geolocation.vue'
-export default {
-  components: {
-    Geolocation
-  }
-
-}
+<script setup>
+import geolocation from "/src/components/geolocation.vue";
 </script>
 
-<style>
+<template>
+	<geolocation />
+</template>
 
+<style scoped>
+.logo {
+	height: 6em;
+	padding: 1.5em;
+	will-change: filterr;
+	transition: filter 300ms;
+}
+.logo:hover {
+	filter: drop-shadow(0 0 2em #646cffaa);
+}r
+.logo.vue:hover {
+	filter: drop-shadow(0 0 2em #42b883aa);
+}
 </style>
--- src/components/geolocation.vue
+++ src/components/geolocation.vue
@@ -1,49 +1,52 @@
 <template>
-    <div>
-        <form @submit.prevent="getcordinates">
-            <input type="text" v-model="address" placeholder="Maringá, PR">
-            <button @type="submit">pesquisar</button>
-        </form>
-        <div v-if="response !={}">
-            <p>{{ response }}</p>
-        </div>
-    </div>
-
+	<div>
+		<form @submit.prevent="getcordinates">
+			<input type="text" v-model="address" placeholder="Maringá, PR" />
+			<button @type="submit">pesquisar</button>
+		</form>
+		<div v-if="response">
+            <p>{{response}}</p>
+			<ul>
+				<li v-for="item in time" :key="item.id">{{ item.id }}{{ item }}</li>
+			</ul>
+		</div>
+	</div>
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 export default {
-    data() {
-        return {
-            address: '',
-            lat: '',
-            lng: '',
-            response: {}
-        }
-    },
-    methods: {
-        async getcordinates() {
-            const response = await axios.get(
-                `https://maps.googleapis.com/maps/api/geocode/json?address=${this.address}&key=AIzaSyAtDRhVstWbo-YnbnO6h64LyxFr0GOridg`
-            )
-            const location = response.data.results[0].geometry.location
-            this.lat = location.lat;
-            this.lng = location.lng
-            const responseForecast = await axios.get(
-                `https://api.open-meteo.com/v1/forecast?latitude=${this.lat}&longitude=${this.lng}&hourly=temperature_2m,relativehumidity_2m,precipitation,rain,weathercode&daily=weathercode,temperature_2m_max,temperature_2m_min,precipitation_sum,precipitation_hours&timezone=America%2FSao_Paulo`
-            )
-            this.response = responseForecast
-            console.log(responseForecast)
-            console.log(JSON.stringify(this.response, null, 2))
-            
-        }
-    }
+	data() {
+		return {
+			address: "",
+			response: null,
+			time: "",
+		};
+	},
+	methods: {
+		async getcordinates() {
+            try {
+                const response = await axios.get(
+                    `https://maps.googleapis.com/maps/api/geocode/json?address=${this.address}&key=AIzaSyAtDRhVstWbo-YnbnO6h64LyxFr0GOridg`
+                );
+                const location = response.data.results[0].geometry.location;
+                let lat = location.lat;
+                let lng = location.lng;
+                let responseForecast = await axios.get(
+                    `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lng}&hourly=temperature_2m,precipitation,weathercode&current_weather=truedaily=weathercode,temperature_2m_max,temperature_2m_min&current_weather=true&timezone=America%2FSao_Paulo`
+                );
+                    this.response = responseForecast;
 
-}
+                // this.time = responseForecast.data.hourly.time
+                console.log(responseForecast.data);
+            }
+            catch {
+                console.log(error)
+            }
 
+            }
+	},
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
